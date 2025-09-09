2ef3afabdc3a304e27f907c4889b7561b7fbe1f6
Chase McLain <chasermclain@gmail.com>
2025-09-09 13:37:06 -0600

    pet schema

--- src/models/Pet.js
+++ src/models/Pet.js
@@ -1,7 +1,21 @@
+import { Schema } from "mongoose";
+
+
+
+export const PetSchema = new Schema({
+  name: { type: String, required: true, minLength: 1, maxLength: 25 },
+  age: { type: Number, required: true, min: 0, max: 99 },
+  isVaccinated: { type: Boolean, required, default: false },
+  species: { type: String, required: true, minLength: 1, maxLength: 25 },
+  imgUrl: { type: String, required: true, minLength: 1, maxLength: 500 },
+  status: { type: String, required: true, minLength: 1, maxLength: 25, default: "adoptable" },
+  like: [{ type: String }],
 
 
 
 
+
+})
 
 
 
